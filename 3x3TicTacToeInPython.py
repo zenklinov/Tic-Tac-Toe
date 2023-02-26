d1ed1216054376dd03bf33b8cf7554e962191583
Amanatullah Pandu Zenklinov <91674858+zenklinov@users.noreply.github.com>
2023-02-27 06:10:03 +0700

    Update 3x3TicTacToeInPython.py

--- 3x3TicTacToeInPython.py
+++ 3x3TicTacToeInPython.py
@@ -1,106 +1,82 @@
-import tkinter as tk
-from tkinter import messagebox
+from tkinter import *
 
-# Set up the game board
-board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-player = 'X'
-
-# Define the function to reset the game
-def reset_game():
-    global board, player
-    board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-    player = 'X'
-    for i in range(9):
-        labels[i].config(text=board[i])
-    status_label.config(text=f"Player {player}'s turn")
-
-# Define the function to handle a player's move
-def player_move(cell):
-    global player
-    if board[cell] == ' ':
-        board[cell] = player
-        labels[cell].config(text=player)
-        # Check for a win or tie
-        if (board[0] == board[1] == board[2] != ' ' or
-            board[3] == board[4] == board[5] != ' ' or
-            board[6] == board[7] == board[8] != ' ' or
-            board[0] == board[3] == board[6] != ' ' or
-            board[1] == board[4] == board[7] != ' ' or
-            board[2] == board[5] == board[8] != ' ' or
-            board[0] == board[4] == board[8] != ' ' or
-            board[2] == board[4] == board[6] != ' '):
-            messagebox.showinfo("Game Over", f"Player {player} wins!")
-            reset_game()
-        elif ' ' not in board:
-            messagebox.showinfo("Game Over", "Tie game!")
-            reset_game()
-        # Switch to the other player's turn
-        player = 'O' if player == 'X' else 'X'
-        status_label.config(text=f"Player {player}'s turn")
-
-# Create the main window
-root = tk.Tk()
-root.title("Tic Tac Toe")
-
-# Create the labels for each cell on the board
-labels = []
-for i in range(3):
-    for j in range(3):
-        label = tk.Label(root, text=board[i*3+j], font=('Arial', 60), width=3, height=1, relief='groove')
-        label.grid(row=i, column=j)
-        labels.append(label)
-
-# Create the "Reset" button
-reset_button = tk.Button(root, text="Reset", font=('Arial', 20), command=reset_game)
-reset_button.grid(row=3, column=0, columnspan=3)
-
-# Create the status label
-status_label = tk.Label(root, text=f"Player {player}'s turn", font=('Arial', 20))
-status_label.grid(row=4, column=0, columnspan=3)
-
-# Bind each label to the player's move function
-for i in range(9):
-    labels[i].bind("<Button-1>", lambda event, i=i: player_move(i))
-
-# Start the game
+class TicTacToe:
+    def __init__(self, master):
+        self.master = master
+        master.title("Tic Tac Toe")
+        
+        self.current_player = "X"
+        self.game_over = False
+        
+        self.create_board()
+        self.create_reset_button()
+        
+    def create_board(self):
+        self.board = [[None for j in range(3)] for i in range(3)]
+        self.buttons = [[None for j in range(3)] for i in range(3)]
+        
+        for i in range(3):
+            for j in range(3):
+                button = Button(self.master, text="", width=4, height=2,
+                                command=lambda row=i, col=j: self.handle_click(row, col))
+                button.grid(row=i, column=j)
+                self.buttons[i][j] = button
+                
+    def create_reset_button(self):
+        reset_button = Button(self.master, text="Reset", command=self.reset)
+        reset_button.grid(row=3, column=1)
+                
+    def handle_click(self, row, col):
+        if self.board[row][col] is None and not self.game_over:
+            self.board[row][col] = self.current_player
+            self.buttons[row][col].config(text=self.current_player)
+            
+            if self.check_win(row, col):
+                self.game_over = True
+                messagebox.showinfo("Game Over", f"{self.current_player} wins!")
+            elif self.check_draw():
+                self.game_over = True
+                messagebox.showinfo("Game Over", "Draw!")
+            else:
+                self.switch_players()
+                
+    def switch_players(self):
+        self.current_player = "O" if self.current_player == "X" else "X"
+        
+    def check_win(self, row, col):
+        # Check row
+        if all(cell == self.current_player for cell in self.board[row]):
+            return True
+        
+        # Check column
+        if all(self.board[i][col] == self.current_player for i in range(3)):
+            return True
+        
+        # Check diagonal
+        if row == col and all(self.board[i][i] == self.current_player for i in range(3)):
+            return True
+        
+        # Check anti-diagonal
+        if row + col == 2 and all(self.board[i][2-i] == self.current_player for i in range(3)):
+            return True
+        
+        return False
+    
+    def check_draw(self):
+        for row in range(3):
+            for col in range(3):
+                if self.board[row][col] is None:
+                    return False
+        return True
+    
+    def reset(self):
+        self.current_player = "X"
+        self.game_over = False
+        for i in range(3):
+            for j in range(3):
+                self.board[i][j] = None
+                self.buttons[i][j].config(text="")
+        
+root = Tk()
+game = TicTacToe(root)
 root.mainloop()
-
-import tkinter as tk
-from tkinter import messagebox
-
-
-class GameState:
-    def __init__(self):
-        self.board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-        self.current_player = 'X'
-
-
-def handle_cell_click(game_state, cell):
-    if game_state.board[cell] == ' ':
-        game_state.board[cell] = game_state.current_player
-        labels[cell].config(text=game_state.current_player)
-        # Check for a win or tie
-        if (game_state.board[0] == game_state.board[1] == game_state.board[2] != ' ' or
-            game_state.board[3] == game_state.board[4] == game_state.board[5] != ' ' or
-            game_state.board[6] == game_state.board[7] == game_state.board[8] != ' ' or
-            game_state.board[0] == game_state.board[3] == game_state.board[6] != ' ' or
-            game_state.board[1] == game_state.board[4] == game_state.board[7] != ' ' or
-            game_state.board[2] == game_state.board[5] == game_state.board[8] != ' ' or
-            game_state.board[0] == game_state.board[4] == game_state.board[8] != ' ' or
-            game_state.board[2] == game_state.board[4] == game_state.board[6] != ' '):
-            messagebox.showinfo("Game Over", f"Player {game_state.current_player} wins!")
-            reset_game(game_state)
-        elif ' ' not in game_state.board:
-            messagebox.showinfo("Game Over", "Tie game!")
-            reset_game(game_state)
-        # Switch to the other player's turn
-        game_state.current_player = 'O' if game_state.current_player == 'X' else 'X'
-        status_label.config(text=f"Player {game_state.current_player}'s turn")
-
-
-def reset_game(game_state):
-    game_state.board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-    game_state.current_player = 'X'
-    for i in range(9):
-        labels[i].config(text=game_state.board[i])
-    status_label.config(text=f"Player {game_state.current_player}'s turn")
